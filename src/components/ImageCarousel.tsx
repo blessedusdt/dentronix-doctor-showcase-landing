d7752651985fbaf1d481a046f87446388d8aeb68
blessedusdt <httpg44b@gmail.com>
2025-04-16 14:10:20 -0300

    new carrossel image

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -27,7 +27,7 @@ const images = [
   {
     src: "/dentist-4.jpg",
     alt: "Sorriso perfeito após tratamento",
-    fallback: "https://images.unsplash.com/photo-1607706189160-ecb490bcbcc2?q=80&w=2576&auto=format&fit=crop"
+    fallback: "https://images.unsplash.com/photo-1631051103633-24959376b92d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   }
 ];
 
